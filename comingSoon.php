19a4553c98f5425c8153dd305971df7163c89116
Fazle Rabbi <rabbifff9@gmail.com>
2023-08-22 11:51:20 +0600

    rty

---
+++ comingSoon.php
@@ -0,0 +1,5 @@
+<?php require ('navigation.php')?>
+<div class="comings">
+    <p>Coming Soon...</p>
+</div>
+<?php require ('footer.php')?>
--- navigation.php
+++ navigation.php
@@ -34,78 +34,78 @@
                     <li class="menu-item" id="sm1"><a href="#">About Us<i class="ri-arrow-drop-down-line"></i></a>
                         <ul class="sub-menu active sm1">
                             <li class="submenuitems"><a href="/profile.php"><i class="ri-arrow-drop-right-fill"></i>Profile</a></li>
-                            <li class="submenuitems"><a href="#"><i class="ri-arrow-drop-right-fill"></i>Vision & Mission</a></li>
-                            <li class="submenuitems"><a href="#"><i class="ri-arrow-drop-right-fill"></i>The Pioneer</a></li>
-                            <li class="submenuitems"><a href="#"><i class="ri-arrow-drop-right-fill"></i>Board of Directors</a></li>
-                            <li class="submenuitems"><a href="#"><i class="ri-arrow-drop-right-fill"></i>Specialities</a></li>
-                            <li class="submenuitems"><a href="#"><i class="ri-arrow-drop-right-fill"></i>Corporate Structure</a></li>
-                            <li class="submenuitems"><a href="#"><i class="ri-arrow-drop-right-fill"></i>Global Presence</a></li>
-                            <li class="submenuitems"><a href="#"><i class="ri-arrow-drop-right-fill"></i>Customer Reviews</a></li>
-                            <li class="submenuitems"><a href="#"><i class="ri-arrow-drop-right-fill"></i>AWARDS</a></li>
-                            <li class="submenuitems"><a href="#"><i class="ri-arrow-drop-right-fill"></i>Affiliations</a></li>
-                            <li class="submenuitems"><a href="#"><i class="ri-arrow-drop-right-fill"></i>CSR</a></li>
-                            <li class="submenuitems"><a href="#"><i class="ri-arrow-drop-right-fill"></i>Land Wanted</a></li>
-                            <li class="submenuitems"><a href="#"><i class="ri-arrow-drop-right-fill"></i>Certifications</a></li>
-                            <li class="submenuitems"><a href="#"><i class="ri-arrow-drop-right-fill"></i>Testimonials</a></li>
+                            <li class="submenuitems"><a href="comingSoon.php"><i class="ri-arrow-drop-right-fill"></i>Vision & Mission</a></li>
+                            <li class="submenuitems"><a href="comingSoon.php"><i class="ri-arrow-drop-right-fill"></i>The Pioneer</a></li>
+                            <li class="submenuitems"><a href="comingSoon.php"><i class="ri-arrow-drop-right-fill"></i>Board of Directors</a></li>
+                            <li class="submenuitems"><a href="comingSoon.php"><i class="ri-arrow-drop-right-fill"></i>Specialities</a></li>
+                            <li class="submenuitems"><a href="comingSoon.php"><i class="ri-arrow-drop-right-fill"></i>Corporate Structure</a></li>
+                            <li class="submenuitems"><a href="comingSoon.php"><i class="ri-arrow-drop-right-fill"></i>Global Presence</a></li>
+                            <li class="submenuitems"><a href="comingSoon.php"><i class="ri-arrow-drop-right-fill"></i>Customer Reviews</a></li>
+                            <li class="submenuitems"><a href="comingSoon.php"><i class="ri-arrow-drop-right-fill"></i>AWARDS</a></li>
+                            <li class="submenuitems"><a href="comingSoon.php"><i class="ri-arrow-drop-right-fill"></i>Affiliations</a></li>
+                            <li class="submenuitems"><a href="comingSoon.php"><i class="ri-arrow-drop-right-fill"></i>CSR</a></li>
+                            <li class="submenuitems"><a href="comingSoon.php"><i class="ri-arrow-drop-right-fill"></i>Land Wanted</a></li>
+                            <li class="submenuitems"><a href="comingSoon.php"><i class="ri-arrow-drop-right-fill"></i>Certifications</a></li>
+                            <li class="submenuitems"><a href="comingSoon.php"><i class="ri-arrow-drop-right-fill"></i>Testimonials</a></li>
                         </ul>
                     </li>
                     <li class="menu-item" id="sm2"><a href="#">Projects<i class="ri-arrow-drop-down-line"></i></a>
                         <ul class="sub-menu active sm2">
-                            <li class="submenuitems"><a href="#"><i class="ri-arrow-drop-right-fill"></i>Ongoing</a></li>
-                            <li class="submenuitems"><a href="#"><i class="ri-arrow-drop-right-fill"></i>Upcoming</a></li>
-                            <li class="submenuitems"><a href="#"><i class="ri-arrow-drop-right-fill"></i>Ready</a></li>
+                            <li class="submenuitems"><a href="comingSoon.php"><i class="ri-arrow-drop-right-fill"></i>Ongoing</a></li>
+                            <li class="submenuitems"><a href="comingSoon.php"><i class="ri-arrow-drop-right-fill"></i>Upcoming</a></li>
+                            <li class="submenuitems"><a href="comingSoon.php"><i class="ri-arrow-drop-right-fill"></i>Ready</a></li>
                         </ul>
                     </li>
                     <li class="menu-item" id="sm3"><a href="#">Concerns<i class="ri-arrow-drop-down-line"></i></a>
                         <ul class="sub-menu active sm3">
-                            <li class="submenuitems"><a href="#"><i class="ri-arrow-drop-right-fill"></i>Soulmate City</a></li>
-                            <li class="submenuitems"><a href="#"><i class="ri-arrow-drop-right-fill"></i>Soulmate Ventures</a></li>
-                            <li class="submenuitems"><a href="#"><i class="ri-arrow-drop-right-fill"></i>Soulmate Properties bazar</a></li>
-                            <li class="submenuitems"><a href="#"><i class="ri-arrow-drop-right-fill"></i>Soulmate Engineering <br> and Consultancy</a></li>
-                            <li class="submenuitems"><a href="#"><i class="ri-arrow-drop-right-fill"></i>Soulmate Developers</a></li>
-                            <li class="submenuitems"><a href="#"><i class="ri-arrow-drop-right-fill"></i>Soulmate Land</a></li>
-                            <li class="submenuitems"><a href="#"><i class="ri-arrow-drop-right-fill"></i>Soulmate Resort</a></li>
-                            <li class="submenuitems"><a href="#"><i class="ri-arrow-drop-right-fill"></i>Soulmate Restaurant<br>and Connoisseur</a></li>
-                            <li class="submenuitems"><a href="#"><i class="ri-arrow-drop-right-fill"></i>Soulmate Assets</a></li>
+                            <li class="submenuitems"><a href="comingSoon.php"><i class="ri-arrow-drop-right-fill"></i>Soulmate City</a></li>
+                            <li class="submenuitems"><a href="comingSoon.php"><i class="ri-arrow-drop-right-fill"></i>Soulmate Ventures</a></li>
+                            <li class="submenuitems"><a href="comingSoon.php"><i class="ri-arrow-drop-right-fill"></i>Soulmate Properties bazar</a></li>
+                            <li class="submenuitems"><a href="comingSoon.php"><i class="ri-arrow-drop-right-fill"></i>Soulmate Engineering <br> and Consultancy</a></li>
+                            <li class="submenuitems"><a href="comingSoon.php"><i class="ri-arrow-drop-right-fill"></i>Soulmate Developers</a></li>
+                            <li class="submenuitems"><a href="comingSoon.php"><i class="ri-arrow-drop-right-fill"></i>Soulmate Land</a></li>
+                            <li class="submenuitems"><a href="comingSoon.php"><i class="ri-arrow-drop-right-fill"></i>Soulmate Resort</a></li>
+                            <li class="submenuitems"><a href="comingSoon.php"><i class="ri-arrow-drop-right-fill"></i>Soulmate Restaurant<br>and Connoisseur</a></li>
+                            <li class="submenuitems"><a href="comingSoon.php"><i class="ri-arrow-drop-right-fill"></i>Soulmate Assets</a></li>
                         </ul>
                     </li>
                     <li class="menu-item" id="sm4"><a href="#">Events<i class="ri-arrow-drop-down-line"></i></a>
                         <ul class="sub-menu active sm4">
-                            <li class="submenuitems"><a href="#"><i class="ri-arrow-drop-right-fill"></i>Project Overview</a></li>
-                            <li class="submenuitems"><a href="#"><i class="ri-arrow-drop-right-fill"></i>Project Animation</a></li>
-                            <li class="submenuitems"><a href="#"><i class="ri-arrow-drop-right-fill"></i>Project Activities & Update</a></li>
-                            <li class="submenuitems"><a href="#"><i class="ri-arrow-drop-right-fill"></i>Project Review</a></li>
-                            <li class="submenuitems"><a href="#"><i class="ri-arrow-drop-right-fill"></i>Project Visit News</a></li>
-                            <li class="submenuitems"><a href="#"><i class="ri-arrow-drop-right-fill"></i>Project News</a></li>
-                            <li class="submenuitems"><a href="#"><i class="ri-arrow-drop-right-fill"></i>Project</a></li>
+                            <li class="submenuitems"><a href="comingSoon.php"><i class="ri-arrow-drop-right-fill"></i>Project Overview</a></li>
+                            <li class="submenuitems"><a href="comingSoon.php"><i class="ri-arrow-drop-right-fill"></i>Project Animation</a></li>
+                            <li class="submenuitems"><a href="comingSoon.php"><i class="ri-arrow-drop-right-fill"></i>Project Activities & Update</a></li>
+                            <li class="submenuitems"><a href="comingSoon.php"><i class="ri-arrow-drop-right-fill"></i>Project Review</a></li>
+                            <li class="submenuitems"><a href="comingSoon.php"><i class="ri-arrow-drop-right-fill"></i>Project Visit News</a></li>
+                            <li class="submenuitems"><a href="comingSoon.php"><i class="ri-arrow-drop-right-fill"></i>Project News</a></li>
+                            <li class="submenuitems"><a href="comingSoon.php"><i class="ri-arrow-drop-right-fill"></i>Project</a></li>
                         </ul>
                     </li>
                     <li class="menu-item" id="sm5"><a href="#">Career<i class="ri-arrow-drop-down-line"></i></a>
                         <ul class="sub-menu active sm5">
-                            <li class="submenuitems"><a href="#"><i class="ri-arrow-drop-right-fill"></i>Corporate Leader</a></li>
-                            <li class="submenuitems"><a href="#"><i class="ri-arrow-drop-right-fill"></i>Construction Engineers</a></li>
-                            <li class="submenuitems"><a href="#"><i class="ri-arrow-drop-right-fill"></i>Business Partners</a></li>
-                            <li class="submenuitems"><a href="#"><i class="ri-arrow-drop-right-fill"></i>Entrepreneurs</a></li>
-                            <li class="submenuitems"><a href="#"><i class="ri-arrow-drop-right-fill"></i>Construction Workers</a></li>
-                            <li class="submenuitems"><a href="#"><i class="ri-arrow-drop-right-fill"></i>Security Manpower</a></li>
-                            <li class="submenuitems"><a href="#"><i class="ri-arrow-drop-right-fill"></i>Contractors</a></li>
-                            <li class="submenuitems"><a href="#"><i class="ri-arrow-drop-right-fill"></i>Vendor & Supplier</a></li>
+                            <li class="submenuitems"><a href="comingSoon.php"><i class="ri-arrow-drop-right-fill"></i>Corporate Leader</a></li>
+                            <li class="submenuitems"><a href="comingSoon.php"><i class="ri-arrow-drop-right-fill"></i>Construction Engineers</a></li>
+                            <li class="submenuitems"><a href="comingSoon.php"><i class="ri-arrow-drop-right-fill"></i>Business Partners</a></li>
+                            <li class="submenuitems"><a href="comingSoon.php"><i class="ri-arrow-drop-right-fill"></i>Entrepreneurs</a></li>
+                            <li class="submenuitems"><a href="comingSoon.php"><i class="ri-arrow-drop-right-fill"></i>Construction Workers</a></li>
+                            <li class="submenuitems"><a href="comingSoon.php"><i class="ri-arrow-drop-right-fill"></i>Security Manpower</a></li>
+                            <li class="submenuitems"><a href="comingSoon.php"><i class="ri-arrow-drop-right-fill"></i>Contractors</a></li>
+                            <li class="submenuitems"><a href="comingSoon.php"><i class="ri-arrow-drop-right-fill"></i>Vendor & Supplier</a></li>
                         </ul>
                     </li>
                     <li class="menu-item" id="sm6"><a href="#">Buy & Sales<i class="ri-arrow-drop-down-line"></i></a>
                         <ul class="sub-menu active sm6">
-                            <li class="submenuitems"><a href="#"><i class="ri-arrow-drop-right-fill"></i>Construction Materials</a></li>
-                            <li class="submenuitems"><a href="#"><i class="ri-arrow-drop-right-fill"></i>Land</a></li>
-                            <li class="submenuitems"><a href="#"><i class="ri-arrow-drop-right-fill"></i>Ready Flat/Ofiice</a></li>
-                            <li class="submenuitems"><a href="#"><i class="ri-arrow-drop-right-fill"></i>Project</a></li>
-                            <li class="submenuitems"><a href="#"><i class="ri-arrow-drop-right-fill"></i>Brokerages</a></li>
+                            <li class="submenuitems"><a href="comingSoon.php"><i class="ri-arrow-drop-right-fill"></i>Construction Materials</a></li>
+                            <li class="submenuitems"><a href="comingSoon.php"><i class="ri-arrow-drop-right-fill"></i>Land</a></li>
+                            <li class="submenuitems"><a href="comingSoon.php"><i class="ri-arrow-drop-right-fill"></i>Ready Flat/Ofiice</a></li>
+                            <li class="submenuitems"><a href="comingSoon.php"><i class="ri-arrow-drop-right-fill"></i>Project</a></li>
+                            <li class="submenuitems"><a href="comingSoon.php"><i class="ri-arrow-drop-right-fill"></i>Brokerages</a></li>
                         </ul>
                     </li>
                     
-                    <li class="menu-item"><a href="#">Blog</a></li>
-                    <li class="menu-item"><a href="#">Galleries</a></li>
-                    <li class="menu-item"><a href="#">News</a></li>
-                    <li class="menu-item"><a href="#">Reviews</a></li>
+                    <li class="menu-item"><a href="comingSoon.php">Blog</a></li>
+                    <li class="menu-item"><a href="comingSoon.php">Galleries</a></li>
+                    <li class="menu-item"><a href="comingSoon.php">News</a></li>
+                    <li class="menu-item"><a href="comingSoon.php">Reviews</a></li>
                     <li class="menu-item"><a href="./contact.php">Contact</a></li>
                </ul>
             </div>
